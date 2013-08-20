5382f0be8394bae9e6088c257e53ba227895dd7c
Isaac Arias <ikester@gmail.com>
2013-08-20 13:32:13 -0400

    Initial site upload

---
+++ js/site.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  $('.navbar .nav > li > a').click(function() {
+    $('.navbar-collapse.navbar-ex1-collapse.in').removeClass('in').addClass('collapse').css('height', '0');
+  });
+  if (iPadVersion != 1) $('#topnav').localScroll({ hash: true });
+  ssm.addStates([
+    {
+        id: 'mobile',
+        width: 767,
+        onEnter: function() {
+        }
+    },
+    {
+        id: 'tablet',
+        width: 979,
+        onEnter: function() {
+        }
+    },
+    {
+        id: 'desktop',
+        width: 9999,
+        onEnter: function() {
+          if (!isiOS()) {
+            $('#about').parallax("30%", 0.5);
+          }
+        }
+    }
+  ]);
+  ssm.ready();
+});
+
+
+function refreshScrollspy() {
+  $('[data-spy="scroll"]').each(function () {
+    var $spy = $(this).scrollspy('refresh')
+  })
+};
+
+var resizeTimer;
+$(window).resize(function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(refreshScrollspy, 100);
+});
+
+function isiOS(){
+  return (
+    //Detect iPad
+    (navigator.platform.indexOf("iPad") != -1) ||
+    //Detect iPhone
+    (navigator.platform.indexOf("iPhone") != -1) ||
+    //Detect iPod
+    (navigator.platform.indexOf("iPod") != -1)
+  );
+}
+
+function iPadVersion() {
+  //Detect iPad 1
+  if (navigator.platform.indexOf("Mobile/8F190") != -1) return 1;
+  //Detect iPad 2
+  else if (navigator.platform.indexOf("Mobile/8F191") != -1) return 2;
+  //Detect iPad 3
+  else if (navigator.platform.indexOf("Mobile/9B176") != -1) return 3;
+  else return 0;
+}
